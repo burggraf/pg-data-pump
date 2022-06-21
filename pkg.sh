8a4e4e6ddfce5a0bb463ce6afd448c4572b51559
Mark Burggraf <markb@mantisbible.com>
2022-06-21 09:00:39 -0700

    renamed index.ts to postgresql-datapump.ts

--- pkg.sh
+++ pkg.sh
@@ -1,4 +1,4 @@
 npm install -g pkg
-tsc index.ts
-pkg index.js -t node16-macos-x64,node16-win-x64,node16-linux-x64,node16-macos-arm64,node16-win-arm64,node16-linux-arm64
+tsc postgresql-datapump.ts
+pkg postgresql-datapump.js -t node16-macos-x64,node16-win-x64,node16-linux-x64,node16-macos-arm64,node16-win-arm64,node16-linux-arm64
 
